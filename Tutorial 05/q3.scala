eec8d986cf5eb6982f5b6db951a800921e5beb96
NadeeshaEpa <91688408+NadeeshaEpa@users.noreply.github.com>
2022-08-23 01:05:51 +0530

    Delete Tutorial 05 directory

--- Tutorial 05/q3.scala
+++
@@ -1,7 +0,0 @@
-object q3 extends App{
-    def add(n:Int):Int=n match{
-       case 0=>0
-       case _ =>n+add(n-1)
-    }
-    print(add(5))
-}
--- Tutorial 05/q4.scala
+++
@@ -1,9 +0,0 @@
-object q4 extends App{
-   def iseven(x:Int):Boolean=x match{
-      case 0=>true
-      case _=>isodd(x-1)
-   }
-   def isodd(x:Int):Boolean= !(iseven(x))
-      
-   println(iseven(21))
-}
