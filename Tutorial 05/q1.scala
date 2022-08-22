d39cafe7e3647aeab1308d3117c4dd7e88748a94
NadeeshaEpa <91688408+NadeeshaEpa@users.noreply.github.com>
2022-08-22 17:01:34 +0530

    q1.scala

---
+++ Tutorial 05/q1.scala
@@ -0,0 +1,16 @@
+object q1 extends App{
+   def gcd(a:Int,b:Int):Int=b match{
+       case 0 => a
+       case x if x>a=>gcd(b,a)
+       case _=>gcd(b,a%b) 
+   }
+  
+   def prime(a:Int,n:Int=2):Boolean =n match{
+     case x if(x==a)=>true
+     case x if gcd(a,x)>1=>false
+     case x=>prime(a,x+1)
+   }
+
+   println(prime(5))
+   println(prime(8))
+}
