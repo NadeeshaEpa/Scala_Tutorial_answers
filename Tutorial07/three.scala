3bbb1dbbb3abf43eafc99ee21490c4168db14332
NadeeshaEpa <91688408+NadeeshaEpa@users.noreply.github.com>
2022-09-12 18:46:06 +0530

    three.scala

--- Tutorial07/three.scala
+++ Tutorial07/three.scala
@@ -1,3 +1,4 @@
+ import scala.io.StdIn.readDouble
  class Account(id:String,n:Int,b:Double){
     val nic:String=id
     val acno=n
@@ -16,13 +17,18 @@
     }
 }
 object q3 extends App{
-  //var bank:List[Account]=List()
   val acc1=new Account("001",123,25000)
   val acc2=new Account("002",234,50000)
+
+  var bank:List[Account]=List(acc1,acc2)
+
   println("Before the transaction:")
   println("Account "+acc1.acno+" balance:"+acc1.balance)
   println("Account "+acc2.acno+" balance:"+acc2.balance+"\n")
-  acc1.transfer(acc2,2000)
+
+  print("Enter the amount you want to transfer:")
+  var amount=readDouble()
+  acc1.transfer(acc2,amount)
   println("After the transaction:")
   println("Account "+acc1.acno+" balance:"+acc1.balance)
   println("Account "+acc2.acno+" balance:"+acc2.balance)
