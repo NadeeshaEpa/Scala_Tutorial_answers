b94ead5da8f85f0f122b6843f5ed9bc0f19d866c
NadeeshaEpa <91688408+NadeeshaEpa@users.noreply.github.com>
2022-09-12 19:08:34 +0530

    Update one.scala

--- Tutorial07/one.scala
+++ Tutorial07/one.scala
@@ -1,10 +1,11 @@
 class Rational(x:Int,y:Int){
+    require(y>0,"denominator must be positive")
     val numer=x
     val denom=y
     def neg=new Rational(-this.numer,this.denom)
 }
 object q2 extends App{
-    val x = new Rational(1, 3)
+    val x = new Rational(1,3)
     val result=x.neg;
     println("Negative value of "+x.numer+"/"+x.denom+" is equal to "+result.numer+"/"+result.denom)
 }
